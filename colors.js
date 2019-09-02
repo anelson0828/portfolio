17be03aa61addc476ae6cd07ca56acbbed9488c4
Amanda <amandamarienelson2@gmail.com>
2019-09-01 20:25:12 -0400

    initial commit

---
+++ colors.js
@@ -0,0 +1,14 @@
+module.exports = {
+  background: '#FFFFFF',
+  backgroundDark: '#00CEFC',
+
+  primary: '#00171F',
+  primaryLight: '#00BFFF',
+  primaryDark: '#007EA7',
+
+  secondary: '#003459',
+  secondaryLight: '#B2B2B2',
+  secondaryDark: '#00171F',
+
+  gray: '#F1F1F1',
+};
